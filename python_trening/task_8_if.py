0ca6a99954f47d491b81ed75a9a60264df6906f9
Pavel <yacoolontac@gmail.com>
2023-04-21 21:47:11 +0300

    ADD  README.md

---
+++ python_trening/task_8_if.py
@@ -0,0 +1,12 @@
+num=int(input())
+if num >=0:
+    print("Число больше или равно 0")
+else:
+    print('Число отрицательное')
+
+# str_1='test'
+# str_2='test1'
+# if str_1 in str_2:
+#     print('ДА')
+# else:
+#     print('НЕТ')
---
+++ python_trening/task_9_if_elif_else.py
@@ -0,0 +1,18 @@
+# num=float(input())
+# if num >0:
+#     print("Число положительное")
+# elif num==0:
+#     print("Ноль")
+# else:
+#     print('Число отрицательное')
+# num=3.4
+# perprint = True
+# if num>0 or perprint:
+#     print('нум положительное число')
+# elif perprint:
+#     print('Печать запрещена')
+i=int(input())
+if 100>=i>=-100:
+    print('+')
+else:
+    print('-')
